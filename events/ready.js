bd8e60871688011cb87d027cd998bae7a97ad743
Anathos <83123402+EAnathos@users.noreply.github.com>
2023-10-11 17:59:09 +0200

    Update ready.js
    Fix exploit, reactions weren't fetched.

--- events/ready.js
+++ events/ready.js
@@ -16,12 +16,25 @@ module.exports = {
     await storedContribution.forEach((b) => contribution.set(b.user_id, b));
 
     const infoChannel = client.channels.cache.get(infoChannelID);
-    await infoChannel.messages.fetch({ limit: 30 });
+    let messages = await infoChannel.messages.fetch({ limit: 30 });
+    messages.forEach((msg) => {
+      msg.reactions.cache.forEach((reaction) => {
+        reaction.users.fetch();
+      });
+    });
 
     const modChannel = client.channels.cache.get(modChannelID);
 
-    modChannel.threads.cache.forEach(async (thread) => { 
-      await thread.messages.fetch({ limit: 16, after: thread.id });
+    modChannel.threads.cache.forEach(async (thread) => {
+      let messages = await thread.messages.fetch({
+        limit: 16,
+        after: thread.id,
+      });
+      messages.forEach((msg) => {
+        msg.reactions.cache.forEach((reaction) => {
+          reaction.users.fetch();
+        });
+      });
     });
 
     console.log(`Ready! Logged in as ${client.user.tag}`);
